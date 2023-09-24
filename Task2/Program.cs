e39d29af909bfbea48a6be2cec48072656b08b2a
marisarze <flypetrov@yandex.ru>
2023-09-24 08:10:02 +0300

    Task 2 done

---
+++ Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿void max_min_report(float a, float b){
+    if (a>b) {
+        Console.WriteLine("Number {0} greater than number {1}", a, b);        
+    } 
+    else if (a<b) 
+    {
+        Console.WriteLine("Number {0} greater than number {1}", b, a);
+    } 
+    else 
+    {
+        Console.WriteLine("Number {0} equal to number {1}", a, b);
+    };
+};
+
+
+float a = 0.0f, b = 0.0f;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please, input numbers");
+Console.WriteLine("first number: ");
+a = Single.Parse(Console.ReadLine());
+Console.WriteLine("second number: ");
+b = Single.Parse(Console.ReadLine());
+max_min_report(a, b);
+
