c05c8c4930ac24f8a0a4462d41ac642582ed3784
marisarze <flypetrov@yandex.ru>
2023-10-19 21:55:04 +0300

    Lesson 9 homework

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -1,13 +1,13 @@
-﻿Console.Write(«Enter number m: «);
-int m = Convert.ToInt32(Console.ReadLine());
-Console.Write(«Enter number n: «);
-int n = Convert.ToInt32(Console.ReadLine());
+﻿using System;
 
-int akkerman(int m, int n)
-{
-if (m == 0) return n + 1;
-else if (n == 0) return akkerman(m — 1, 1);
-else return akkerman(m — 1, akkerman(m, n — 1));
+int akkerman(int m, int n){
+    if (m==0) return n+1;
+    else if (n==0) return akkerman(m-1, 1);
+    else return akkerman(m-1, akkerman(m,n-1));
 }
 
-Console.Write($»Akkerman function for {m} and {n} is: {akkerman(m, n)} «);
+Console.Write("Enter number m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write($"function Akkerman({m},{n}) = {akkerman(m, n)}");
