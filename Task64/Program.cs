c1239487bd4857a8eee2c3e3ffcb80f680fa4eb4
marisarze <flypetrov@yandex.ru>
2023-10-19 09:37:57 +0300

    Workin on akkerman algo

---
+++ Task64/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+void PrintNaturalNumbers(int n){
+    if (n!=1){
+        Console.Write($"{n}, ");
+        PrintNaturalNumbers(n-1);
+    } else {
+        Console.WriteLine("1");
+    }
+}
+
+
+Console.WriteLine("Enter number N:");
+int n = Convert.ToInt32(Console.ReadLine());
+PrintNaturalNumbers(n);
---
+++ Task66/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+int GetSumBetween(int M, int N){
+    if (M==N){
+        return M;
+    } else if (M>N){
+        return M + GetSumBetween(M-1, N);
+    } else {
+        return N + GetSumBetween(M, N-1);
+    }
+}
+
+
+Console.WriteLine("Enter number M:");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number N:");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Sum of all numbers from {0} to {1} is {2}", M, N, GetSumBetween(M,N));
