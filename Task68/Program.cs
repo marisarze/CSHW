9381bbff777052cf9628762cda7d1a9a9f29df85
marisarze <flypetrov@yandex.ru>
2023-10-19 09:42:17 +0300

    Workin on akkerman algo continue

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write(«Enter number m: «);
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write(«Enter number n: «);
+int n = Convert.ToInt32(Console.ReadLine());
+
+int akkerman(int m, int n)
+{
+if (m == 0) return n + 1;
+else if (n == 0) return akkerman(m — 1, 1);
+else return akkerman(m — 1, akkerman(m, n — 1));
+}
+
+Console.Write($»Akkerman function for {m} and {n} is: {akkerman(m, n)} «);
